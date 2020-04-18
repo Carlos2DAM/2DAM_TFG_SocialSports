81830991efa8ca22a468bdbc32992df101344bd3
Carlos <2a_dam20_05@iesjovellanos.org>
2020-04-19 01:09:45 +0200

    Editando Pestaña Datos Especificos
    Editando los elementos necesarios en la pestaña datos específicos para crear un nuevo evento.

--- Carlos_Proyecto_AndroidStudio/app/src/main/java/diazhernan/carlos/socialsports/LoginActivity.java
+++ Carlos_Proyecto_AndroidStudio/app/src/main/java/diazhernan/carlos/socialsports/LoginActivity.java
@@ -1,8 +1,6 @@
 package diazhernan.carlos.socialsports;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,10 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import diazhernan.carlos.socialsports.MainActivity;
-import diazhernan.carlos.socialsports.R;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -38,14 +32,14 @@ public class LoginActivity extends AppCompatActivity {
         usernameEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                cambiarColoresTexto((EditText) v);
+                Funcionalidades.cambiarColoresTexto((EditText) v, getApplication());
             }
         });
 
         passwordEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                cambiarColoresTexto((EditText) v);
+                Funcionalidades.cambiarColoresTexto((EditText) v, getApplication());
             }
         });
 
@@ -62,7 +56,7 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
-                cambiarColoresBoton((Button) v);
+                Funcionalidades.cambiarColoresBoton((Button) v, getApplication());
             }
         });
 
@@ -74,28 +68,6 @@ public class LoginActivity extends AppCompatActivity {
                 loadingProgressBar.setVisibility(View.GONE);
             }
         });
-    }
-
-    private void cambiarColoresTexto(EditText et){
-        if (et.isFocused()) {
-            et.setTextColor(getResources().getColor(R.color.colorAccent));
-            et.getBackground().setTint(getResources().getColor(R.color.colorAccent));
-        }
-        else {
-            et.setTextColor(getResources().getColor(R.color.colorElements));
-            et.getBackground().setTint(getResources().getColor(R.color.colorElements));
-        }
-    }
-
-    private void cambiarColoresBoton(Button button){
-        if (button.isFocused()) {
-            button.setTextColor(getResources().getColor(R.color.colorAccent));
-            button.setBackground(getResources().getDrawable(R.drawable.boton1_selected));
-        }
-        else {
-            button.setTextColor(getResources().getColor(R.color.colorElements));
-            button.setBackground(getResources().getDrawable(R.drawable.boton1));
-        }
     }
 
     private boolean comprobarDatosLoginCorrectos(){
@@ -111,8 +83,4 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public static void mostrarMensaje(String mensaje, Context context){
-        Toast toast = Toast.makeText(context,mensaje,Toast.LENGTH_LONG);
-        toast.show();
-    }
 }
--- Carlos_Proyecto_AndroidStudio/app/src/main/java/diazhernan/carlos/socialsports/fragments/newevent/NewEvent.java
+++ Carlos_Proyecto_AndroidStudio/app/src/main/java/diazhernan/carlos/socialsports/fragments/newevent/NewEvent.java
@@ -1,24 +1,20 @@
 package diazhernan.carlos.socialsports.fragments.newevent;
 
-
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 
-import diazhernan.carlos.socialsports.LoginActivity;
+import diazhernan.carlos.socialsports.Funcionalidades;
 import diazhernan.carlos.socialsports.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class NewEvent extends Fragment {
 
     private NewEventDescription newEventDescription = new NewEventDescription();
@@ -26,6 +22,9 @@ public class NewEvent extends Fragment {
     private NewEventRequirements newEventRequirements = new NewEventRequirements();
     private NewEventInvite newEventInvite =new NewEventInvite();
     private TabLayout tabLayout;
+    private Button createButton;
+    private Button nextButton;
+    private Button previousButton;
 
     public NewEvent() {
     }
@@ -38,37 +37,99 @@ public class NewEvent extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        Funcionalidades.showSelectedFragment(R.id.newEventContainer,getActivity().getSupportFragmentManager(),newEventDescription);
+        createButton = getActivity().findViewById(R.id.createtNewEventButton);
+        nextButton = getActivity().findViewById(R.id.buttonNext);
+        previousButton = getActivity().findViewById(R.id.buttonPrevious);
         tabLayout = getActivity().findViewById(R.id.tabs);
-        showSelectedFragment(newEventDescription);
+        activarBotonNext();
+
+        createButton.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                Funcionalidades.cambiarColoresBoton((Button)v,getActivity().getApplication());
+            }
+        });
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setFocusableInTouchMode(true);
+                v.requestFocus();
+                v.setFocusableInTouchMode(false);
+            }
+        });
+
+        nextButton.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                Funcionalidades.cambiarColoresBotonSimple((Button) v,getActivity().getApplication());
+            }
+        });
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setFocusableInTouchMode(true);
+                v.requestFocus();
+                v.setFocusableInTouchMode(false);
+                tabLayout.getTabAt(tabLayout.getSelectedTabPosition()+1).select();
+            }
+        });
+
+        previousButton.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                Funcionalidades.cambiarColoresBotonSimple((Button) v,getActivity().getApplication());
+            }
+        });
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setFocusableInTouchMode(true);
+                v.requestFocus();
+                v.setFocusableInTouchMode(false);
+                tabLayout.getTabAt(tabLayout.getSelectedTabPosition()-1).select();
+            }
+        });
+
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-                if (tab.getText().toString().equals(getResources().getString(R.string.tab_description)))
-                    showSelectedFragment(newEventDescription);
-                else if (tab.getText().toString().equals(getResources().getString(R.string.tab_specify)))
-                    showSelectedFragment(newEventSpecify);
-                else if (tab.getText().toString().equals(getResources().getString(R.string.tab_requirements)))
-                    showSelectedFragment(newEventRequirements);
-                else if (tab.getText().toString().equals(getResources().getString(R.string.tab_invite)))
-                    showSelectedFragment(newEventInvite);
+                if (tab.getText().toString().equals(getResources().getString(R.string.tab_description))) {
+                    Funcionalidades.showSelectedFragment(R.id.newEventContainer,getActivity().getSupportFragmentManager(),newEventDescription);
+                    activarBotonNext();
+                }
+                else if (tab.getText().toString().equals(getResources().getString(R.string.tab_specify))) {
+                    Funcionalidades.showSelectedFragment(R.id.newEventContainer, getActivity().getSupportFragmentManager(), newEventSpecify);
+                    activarBotonNextPrevious();
+                }
+                else if (tab.getText().toString().equals(getResources().getString(R.string.tab_requirements))) {
+                    Funcionalidades.showSelectedFragment(R.id.newEventContainer, getActivity().getSupportFragmentManager(), newEventRequirements);
+                    activarBotonNextPrevious();
+                }
+                else if (tab.getText().toString().equals(getResources().getString(R.string.tab_invite))) {
+                    Funcionalidades.showSelectedFragment(R.id.newEventContainer, getActivity().getSupportFragmentManager(), newEventInvite);
+                    activarBotonPrevious();
+                }
             }
 
             @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabUnselected(TabLayout.Tab tab) {}
 
             @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
+            public void onTabReselected(TabLayout.Tab tab) {}
         });
     }
 
-    //Metodo para elegir que fragment mostrar en pantalla.
-    private void showSelectedFragment(Fragment fragment) {
-        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.homeContainer,fragment)
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+    private void activarBotonNext(){
+        nextButton.setVisibility(View.VISIBLE);
+        previousButton.setVisibility(View.GONE);
     }
-
+    private void activarBotonPrevious(){
+        nextButton.setVisibility(View.GONE);
+        previousButton.setVisibility(View.VISIBLE);
+    }
+    private void activarBotonNextPrevious(){
+        nextButton.setVisibility(View.VISIBLE);
+        previousButton.setVisibility(View.VISIBLE);
+    }
 }
